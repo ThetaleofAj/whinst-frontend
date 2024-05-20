e19facabf3c8aaa80eefc3da7f0d6476d8b13c3b
whenceIT <whenceit@gmail.com>
2024-05-20 13:32:01 +0200

    running tests 2

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,32 @@
 import BlogClient from "../components/blog_client";
+'use client';
+import { useRouter } from 'next/navigation'
+import { initializePaddle, Paddle } from '@paddle/paddle-js';
+import { useEffect, useState } from 'react';
+
+
 
 
 export default function Blog(){
+    const [paddle, setPaddle] = useState<Paddle>();
+    const AUTH_TOKEN =  process.env.WHINST_TEST_API_KEY!
+    const router = useRouter()
+
+    useEffect(()=>{
+        initializePaddle({ environment:'production', token:AUTH_TOKEN,eventCallback(event) { //production
+           if(event.name == "checkout.completed")  {
+             router.refresh()
+           }
+         }, }).then(
+           (paddleInstance: Paddle | undefined) => {
+             if (paddleInstance) {
+               setPaddle(paddleInstance);
+             }
+           },
+         );
+   
+   },[])
+
     return(
     <BlogClient/>
     )
