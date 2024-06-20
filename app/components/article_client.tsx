34ec833cefc7b5d3278b6553e6eb4468598f5490
whenceIT <whenceit@gmail.com>
2024-06-20 22:19:20 +0200

    changes to the blog

--- app/components/article_client.tsx
+++ app/components/article_client.tsx
@@ -11,6 +11,7 @@ import Image from 'next/image'
 export default function ArticleClient(){
 const params = useParams()
 const [blog,setBlog] = useState('' as any);
+const [contents,setContents] = useState([] as any);
 const [isLoading,setIsLoading] = useState(false);
 const [isOpen,setIsOpen] = useState(false)
 
@@ -35,6 +36,8 @@ useEffect(()=>{
         }).then((response)=>response.json())
         .then((json)=>{
           setBlog(json)
+          setContents(json.content)
+
           setIsLoading(false)
         }).catch((error)=>{
           setIsLoading(false)
@@ -119,7 +122,7 @@ useEffect(()=>{
           isLoading ? (<>
             <div className="flex items-center justify-center">
                     <div role="status">
-          <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-black" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin fill-black" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
               <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
           </svg>
@@ -127,20 +130,58 @@ useEffect(()=>{
       </div>
               </div>
           </>):(<>
-            <div className="container flex flex-col items-center gap-12 mt-5 mb-5" key={blog.id}>
+            <div className="container flex flex-col items-center gap-12 mt-5 mb-5 " key={blog.id}>
       <img className='object-cover' src={blog.image} alt=""/>
       <div className='flex flex-col items-center'>
       <h1 className='font-bold text-4xl my-1 pt-5'>{blog.title}</h1>
-      <p className="text-gray-400 dark:text-gray-400">{moment(blog.datePosted).format('LL')}</p>
+      <p className="text-gray-400">{moment(blog.datePosted).format('LL')}</p>
       </div>
-      <div className="prose">
+      {
+        contents.map((content:any)=><>
+        {
+            content.contenttype == '1' ? (<div className='prose'>
+        <Markdown>
+            {content.text}
+        </Markdown>
+            </div>):(<>
+            {
+                content.contenttype == '2' ? (<div className="prose">
+                  <img className='object-cover' src={content.image} alt=""/>
+                </div>):(<>
+                {
+                    content.contenttype == '3' ? (<>
+
+<div className="max-full p-6 bg-[#FFFFED] border rounded-lg shadow border-t-4 border-t-[#ffb31f]">
+<div className="prose">
+                        <Markdown>
+                         {content.banner}
+                        </Markdown>
+                    </div>
+    <a href="#" className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-center font-bold text-white bg-[#ffb31f] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+       <p className='font-bold text-base'>Learn more</p>
+        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+    </a>
+</div>
+
+                    </>):(<></>)
+                }
+                </>)
+            }
+            </>)
+        }
+        </>
+        )
+      }
+      {/* <div className="prose">
 
        <Markdown >
         {blog.content}
        </Markdown>
        
      
-      </div>
+      </div> */}
       </div>
           </>)
       }
