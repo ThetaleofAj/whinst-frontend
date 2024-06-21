b1460c2f715804f2efdb512901707bb2ac33622a
whenceIT <whenceit@gmail.com>
2024-06-21 13:14:07 +0200

    removed blue highlight from learn more

--- app/components/article_client.tsx
+++ app/components/article_client.tsx
@@ -157,7 +157,7 @@ useEffect(()=>{
                          {content.banner}
                         </Markdown>
                     </div>
-    <a href="/" className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-center font-bold text-white bg-[#ffb31f] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+    <a href="/" className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-center font-bold text-white bg-[#ffb31f] rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 ">
        <p className='font-bold text-base'>Learn more</p>
         <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
